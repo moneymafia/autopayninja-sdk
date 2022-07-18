bef64fbca26a8f552d7e1fdae68239f3ce6bbf95
besoeasy <besoeasy@users.noreply.github.com>
2022-07-18 09:14:31 +0530

    -------

--- mock.js
+++ mock.js
@@ -1,4 +1,4 @@
-const { getlink, userpendingtime, subscriptions, canuserpay, usertokenInfo, fetchMyids, totalids } = require('./index');
+const { getlink, subscriptions, canuserpay, usertokenInfo, fetchMyids } = require('./index');
 
 async function test() {
     var a = await getlink('0x8b41e67D6968327664Cf9313b136A0B076000214', '0x8b41e67D6968327664Cf9313b136A0B076000214', '1', '1');
@@ -14,9 +14,6 @@ async function test() {
     console.log(a);
 
     var a = await fetchMyids('0x23Ed8Bbed4FD61d25189CA921448fE15F1B04301');
-    console.log(a);
-
-    var a = await fetchMyids('0x000000000000000000000000000000000000dEaD');
     console.log(a);
 }
 

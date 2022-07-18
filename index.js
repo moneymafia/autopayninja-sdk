5f09330299b5641475cfb460a3966a742b0fe379
besoeasy <besoeasy@users.noreply.github.com>
2022-07-18 09:05:24 +0530

    removed garbage

--- index.js
+++ index.js
@@ -1,101 +1,87 @@
-const abi = require('./contract/abi.json')
+const abi = require('./contract/abi.json');
 
-const ethers = require('ethers')
+const ethers = require('ethers');
 
-const provide = new ethers.providers.EtherscanProvider('ropsten')
+const provide = new ethers.providers.EtherscanProvider('ropsten');
 
-const contract_work = new ethers.Contract("0x15068063F353D946462BCEb9464A8Dce23B9814d", abi, provide)
+const contract_work = new ethers.Contract('0x15068063F353D946462BCEb9464A8Dce23B9814d', abi, provide);
+
+const secondsinaDay = 60;
 
 async function totalids() {
-    var data = await contract_work.sub_index();
+	var data = await contract_work.sub_index();
 
-    return data
-}
-
-async function userpendingtime(_id) {
-    var data = await contract_work.pending_secs(_id);
-
-    return { "seconds": data.toString(), "days": data.div(60).toString() }
+	return data;
 }
 
 async function subscriptions(_id) {
-    var data = await contract_work.subscriptions(_id);
-    var datax = await contract_work.subsalive(_id);
+	var data = await contract_work.subscriptions(_id);
+	var datax = await contract_work.subsalive(_id);
+	var datay = await contract_work.pending_secs(_id);
 
-    var valid = false;
-    if (data.cost.toString() > 0) {
-        valid = true;
-    } else { valid = false; }
+	var valid = false;
+	if (data.cost.toString() > 0) {
+		valid = true;
+	} else {
+		valid = false;
+	}
 
-
-    return { "id": _id.toString(), "token": data.token.toString(), "owner": data.owner.toString(), "merchant": data.merchant.toString(), "cost": data.cost.toString(), "lastpaid": datax.toString(), "valid": valid }
+	return {
+		id: _id.toString(),
+		token: data.token.toString(),
+		owner: data.owner.toString(),
+		merchant: data.merchant.toString(),
+		cost: data.cost.toString(),
+		lastpaid_sec: datax.toString(),
+		unpaid_sec: datay.toString(),
+		unpaid_day: datay.div(secondsinaDay).toString(),
+		unpaid_cost: (data.cost * datay.div(secondsinaDay)).toString(),
+		valid: valid,
+	};
 }
-
 
 async function canuserpay(_id, _days) {
-    var data = await contract_work.canuserpay(_id, _days);
-    return data.toString()
+	var data = await contract_work.canuserpay(_id, _days);
+	return data.toString();
 }
-
 
 async function usertokenInfo(_token, _user) {
-    var datax = await contract_work.balance_user(_user, _token);
-    var datap = await contract_work.allowance(_user, _token);
+	if (ethers.utils.isAddress(_token) && ethers.utils.isAddress(_user)) {
+		var datax = await contract_work.balance_user(_user, _token);
+		var datap = await contract_work.allowance(_user, _token);
 
-    return { "balance": datax.toString(), "allowance": datap.toString() }
+		return { balance: datax.toString(), allowance: datap.toString() };
+	}
 }
-
 
 async function fetchMyids(_user) {
-    var max = await totalids();
+	var max = await totalids();
 
-    let array_u = [];
+	let array_u = [];
 
-    let array_m = [];
+	let array_m = [];
 
-    for (let index = 0; index < max; index++) {
-        var datax = await subscriptions(index);
+	for (let index = 0; index < max; index++) {
+		var datax = await subscriptions(index);
 
-        if (datax.valid) {
-            if (datax.owner == _user) {
-                array_u.push(datax);
-            }
-            if (datax.merchant == _user) {
-                array_m.push(datax);
-            }
+		if (datax.valid) {
+			if (datax.owner == _user) {
+				array_u.push(index);
+			}
+			if (datax.merchant == _user) {
+				array_m.push(index);
+			}
+		}
+	}
 
-        }
-
-    }
-
-    return { "user": array_u, "merchant": array_m }
-}
-
-async function substodays(_id) {
-    var datax = await subscriptions(_id);
-    return { "day": (datax.cost * 1).toString(), "week": (datax.cost * 7).toString(), "month": (datax.cost * 30).toString() }
-}
-
-async function calc_subs_month(_cost) {
-    var daily_cost = _cost / 30;
-
-    return daily_cost
-}
-
-async function calc_subs_week(_cost) {
-    var daily_cost = _cost / 7;
-
-    return daily_cost
+	return { user: array_u, merchant: array_m };
 }
 
 async function getlink(_merchant, _token, _cost, _initdays) {
-
-    if (ethers.utils.isAddress(_merchant) && ethers.utils.isAddress(_token)) {
-        return `https://google.com/join?merchant=${_merchant}&token=${_token}&cost=${_cost}&initdays=${_initdays}`;
-    }
-    return null;
-
+	if (ethers.utils.isAddress(_merchant) && ethers.utils.isAddress(_token)) {
+		return `https://google.com/join?merchant=${_merchant}&token=${_token}&cost=${_cost}&initdays=${_initdays}`;
+	}
+	return null;
 }
 
-module.exports = { getlink, calc_subs_month, calc_subs_week, substodays, userpendingtime, subscriptions, canuserpay, usertokenInfo, fetchMyids, totalids, abi }
-
+module.exports = { getlink, subscriptions, canuserpay, usertokenInfo, fetchMyids, totalids, abi };
--- mock.js
+++ mock.js
@@ -1,37 +1,23 @@
-const { getlink, calc_subs_month, calc_subs_week, substodays, userpendingtime, subscriptions, canuserpay, usertokenInfo, fetchMyids, totalids } = require("./index")
-
+const { getlink, userpendingtime, subscriptions, canuserpay, usertokenInfo, fetchMyids, totalids } = require('./index');
 
 async function test() {
-    var a = await getlink("0x8b41e67D6968327664Cf9313b136A0B076000214", "0x8b41e67D6968327664Cf9313b136A0B076000214", "1", "1");
-    console.log(a)
-
-    var a = await calc_subs_month(15);
-    console.log(a)
-
-    var a = await calc_subs_week(14);
-    console.log(a)
-
-    var a = await substodays(0);
-    console.log(a)
-
-    var a = await userpendingtime(0);
-    console.log(a)
+    var a = await getlink('0x8b41e67D6968327664Cf9313b136A0B076000214', '0x8b41e67D6968327664Cf9313b136A0B076000214', '1', '1');
+    console.log(a);
 
     var a = await subscriptions(0);
-    console.log(a)
+    console.log(a);
 
     var a = await canuserpay(0, 4);
-    console.log(a)
+    console.log(a);
 
-    var a = await usertokenInfo("0x55349E0B114d305f94Cc1cbb2f574e7B5bEcDBd9", "0x23Ed8Bbed4FD61d25189CA921448fE15F1B04301");
-    console.log(a)
+    var a = await usertokenInfo('0x55349E0B114d305f94Cc1cbb2f574e7B5bEcDBd9', '0x23Ed8Bbed4FD61d25189CA921448fE15F1B04301');
+    console.log(a);
 
-    var a = await fetchMyids("0x23Ed8Bbed4FD61d25189CA921448fE15F1B04301");
-    console.log(a)
+    var a = await fetchMyids('0x23Ed8Bbed4FD61d25189CA921448fE15F1B04301');
+    console.log(a);
 
-    var a = await fetchMyids("0x000000000000000000000000000000000000dEaD");
-    console.log(a)
-
+    var a = await fetchMyids('0x000000000000000000000000000000000000dEaD');
+    console.log(a);
 }
 
-test()
+test();

f6b2c32822ee5bc6eb974adcd765077eb4abff19
besoeasy <besoeasy@users.noreply.github.com>
2022-07-19 09:14:21 +0530

    ok

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const networks = {
 		graph: 'https://api.thegraph.com/subgraphs/name/moneymafia/autopayninja',
 		rpc: 'https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
 	},
-	54: {
+	56: {
 		contract: '0x15068063F353D946462BCEb9464A8Dce23B9814d',
 		graph: 'https://api.thegraph.com/subgraphs/name/moneymafia/autopayninja',
 		rpc: 'https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
